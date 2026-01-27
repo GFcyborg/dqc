d369d85fff8ffcb504424a817e93c233ecda4968
Giacomo Fagioli <gf@gfcyb.org>
2026-01-27 16:11:49 +0100

    parsing an openQASM file instead of an hard-coded string

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,19 +1,17 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.nio.file.Paths;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CharStream input = CharStreams.fromString("""
-            OPENQASM 3.0;
-            qubit q;
-        """);
+        CharStream input = CharStreams.fromFileName(Paths.get("app/src/main/openqasm/my.qasm").toString());
 
         qasm3Lexer lexer = new qasm3Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         qasm3Parser parser = new qasm3Parser(tokens);
 
         ParseTree tree = parser.program(); // entry rule
-        System.out.println("Parsed successfully: \n" + tree.toStringTree(parser) );
+        System.out.println("\nParsed successfully:\n" + tree.toStringTree(parser) );
     }
 }
--- app/src/main/openqasm/my.qasm
+++ app/src/main/openqasm/my.qasm
@@ -1,2 +1,3 @@
 OPENQASM 3.0;
-qubit q;
+qubit q1;
+qubit q2;
